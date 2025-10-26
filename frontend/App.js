30559de2329618bad1294198fde2884109e14239
fearghus kennedy <2642922k@student.gla.ac.uk>
2025-10-26 20:29:27 +0000

    expo setup

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import ProcessingScreen from "./screens/ProcessingScreen"; 
+import ResultScreen from "./screens/ResultScreen"
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,16 @@ export default function App() {
           component={HomeScreen}
           options={{ title: "Music Reader" }}
         />
+        <Stack.Screen
+          name="Processing"
+          component={ProcessingScreen}
+          options={{ title: "Processing" }}
+        />
+        <Stack.Screen 
+          name="Result"
+          component={ResultScreen} 
+          options={{ title: "Result" }} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
